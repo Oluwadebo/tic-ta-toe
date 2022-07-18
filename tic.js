777a91b457515a30cbd41f38b1b755182e39bed8
debo <ogunweoluwadebo@gmail.com>
2022-07-18 11:40:38 +0100

    checking username sucefully

--- tic.js
+++ tic.js
@@ -8,31 +8,41 @@ let sevens = document.getElementById("seven");
 let eights = document.getElementById("eight");
 let nines = document.getElementById("nine");
 
+let users = new Audio('Teni_-_Hustle_Mp3bullet.ng.mp3')
 const pla = ()=>{ 
-    let users = new Audio('Teni_-_Hustle_Mp3bullet.ng.mp3')
     users.play();
 }
 let ini = "X";
-const sa = (e)=>{
-    if (ini == "X" &&  e.target.innerText == "") {
-        e.target.innerText = "X";
-        ini = "O";
-    }else if (ini == "O" &&  e.target.innerText == "") {
-        e.target.innerText = "O";
-        ini = "X";
+const starts = ()=>{
+    if (player1.value != "" && player2.value != "") {
+        playr1.innerText = `${player1.value} you are player 1`
+        playr2.innerText = `${player2.value} you are player 2`
+    }else{
+        ready.innerText = "Please provide your name"
     }
-    let a = ones.innerHTML;
-    let b = twos.innerHTML;
-    let c = threes.innerHTML;
-    let d = fours.innerHTML;
-    let f = fives.innerHTML;
-    let g = sixs.innerHTML;
-    let h = sevens.innerHTML;
-    let i = eights.innerHTML;
-    let j = nines.innerHTML;
-    if ((a == "X" && b == "X" && c == "X") || (d == "X" && f == "X" && g == "X") || (h == "X" && i == "X" && j == "X") || (a == "X" && d == "X" && h == "X") || (b == "X" && f == "X" && i == "X") || (c == "X" && g == "X" && j == "X")) {
-        display.innerText = "You win"
-    }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O")) {
-        display.innerText = "You win"
+}
+const sa = (e)=>{
+    if (player1.value != "" && player2.value != ""){
+        if (ini == "X" &&  e.target.innerText == "") {
+            e.target.innerText = "X";
+            ini = "O";
+        }else if (ini == "O" &&  e.target.innerText == "") {
+            e.target.innerText = "O";
+            ini = "X";
+        }
+        let a = ones.innerHTML;
+        let b = twos.innerHTML;
+        let c = threes.innerHTML;
+        let d = fours.innerHTML;
+        let f = fives.innerHTML;
+        let g = sixs.innerHTML;
+        let h = sevens.innerHTML;
+        let i = eights.innerHTML;
+        let j = nines.innerHTML;
+        if ((a == "X" && b == "X" && c == "X") || (d == "X" && f == "X" && g == "X") || (h == "X" && i == "X" && j == "X") || (a == "X" && d == "X" && h == "X") || (b == "X" && f == "X" && i == "X") || (c == "X" && g == "X" && j == "X") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
+            display.innerText = "You win"
+        }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
+            display.innerText = "You win"
+        }           
     }
 }
