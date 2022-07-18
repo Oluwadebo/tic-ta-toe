5efc75861139322fbfcde3206708b818f7dc0dd9
debo <ogunweoluwadebo@gmail.com>
2022-07-18 16:20:09 +0100

    bootsrapt add

--- tic.js
+++ tic.js
@@ -1,13 +1,3 @@
-let ones = document.getElementById("one");
-let twos = document.getElementById("two");
-let threes = document.getElementById("three");
-let fours = document.getElementById("four");
-let fives = document.getElementById("five");
-let sixs = document.getElementById("six");
-let sevens = document.getElementById("seven");
-let eights = document.getElementById("eight");
-let nines = document.getElementById("nine");
-
 const pla = ()=>{ 
     var ayme = document.getElementById("myAduio")
     var isPlaying = false;
@@ -20,21 +10,35 @@ const pla = ()=>{
         ay.innerText = "pause music";
     }
 }
-let ini = "X";
+let ones = document.getElementById("one");
+let twos = document.getElementById("two");
+let threes = document.getElementById("three");
+let fours = document.getElementById("four");
+let fives = document.getElementById("five");
+let sixs = document.getElementById("six");
+let sevens = document.getElementById("seven");
+let eights = document.getElementById("eight");
+let nines = document.getElementById("nine");
+
 const restarts = ()=>{
     let anser = confirm('Are you sure u want to restart');
     if (anser === true) {
         location.reload();
     }
 }
+
 const starts = ()=>{
     if (player1.value != "" && player2.value != "") {
         playr1.innerText = `${player1.value} you are player 1`
         playr2.innerText = `${player2.value} you are player 2`
+        nam1.innerHTML = `${player1.value}`
+        nam2.innerHTML = `${player2.value}`
     }else{
         ready.innerText = "Please provide your name"
     }
 }
+
+let ini = "X";
 const sa = (e)=>{
     if (player1.value != "" && player2.value != ""){
         if (ini == "X" &&  e.target.innerText == "") {
@@ -53,22 +57,36 @@ const sa = (e)=>{
         let h = sevens.innerHTML;
         let i = eights.innerHTML;
         let j = nines.innerHTML;
+        // check winning
         if ((a == "X" && b == "X" && c == "X") || (d == "X" && f == "X" && g == "X") || (h == "X" && i == "X" && j == "X") || (a == "X" && d == "X" && h == "X") || (b == "X" && f == "X" && i == "X") || (c == "X" && g == "X" && j == "X") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
             let p1=0;
             p1 = 0 + 1;
             display.innerText = `${player1.value} you win this round`
             ini = "";
             playr11.innerHTML = p2;
+            resect();
         }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O") || (a == "O" && f == "O" && j == "O") || (c == "O" && f == "O" && h == "O")) {
+            display.innerText = `${player2.value} you win this round`;
             let p2=0;
             p2 = 0 + 1;
-            display.innerText = `${player2.value} you win this round`;
             ini = "";
             playr22.innerHTML = p2;
 
         }
-
     }else{
         ready.innerText = "Please provide your name"
     }
+}
+const resect = ()=>{
+    // disable after winning
+    ini = "X";
+    ones.innerHTML = "";
+    twos.innerHTML  = "";
+    threes.innerHTML  = "";
+    fours.innerHTML  = "";
+    fives.innerHTML  = "";
+    sixs.innerHTML  = "";
+    sevens.innerHTML  = "";
+    eights.innerHTML  = "";
+    nines.innerHTML  = "";
 }
