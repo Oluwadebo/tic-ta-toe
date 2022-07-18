20935b34658ca2909aa8e28a0514e1fe02c75180
debo <ogunweoluwadebo@gmail.com>
2022-07-18 13:45:33 +0100

    stop after a win

--- tic.js
+++ tic.js
@@ -8,9 +8,19 @@ let sevens = document.getElementById("seven");
 let eights = document.getElementById("eight");
 let nines = document.getElementById("nine");
 
-let users = new Audio('Teni_-_Hustle_Mp3bullet.ng.mp3')
+
 const pla = ()=>{ 
-    users.play();
+    var ayme = document.getElementById("myAduio")
+    var isPlaying = false;
+    if (isPlaying) {
+        ayme.pause();
+        ay.innerText = "play music";
+    }
+    else{
+        ayme.play();
+        ay.innerText = "pause music";
+    }
+    isPlaying = true;
 }
 let ini = "X";
 const starts = ()=>{
@@ -40,9 +50,21 @@ const sa = (e)=>{
         let i = eights.innerHTML;
         let j = nines.innerHTML;
         if ((a == "X" && b == "X" && c == "X") || (d == "X" && f == "X" && g == "X") || (h == "X" && i == "X" && j == "X") || (a == "X" && d == "X" && h == "X") || (b == "X" && f == "X" && i == "X") || (c == "X" && g == "X" && j == "X") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
-            display.innerText = "You win"
-        }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
-            display.innerText = "You win"
-        }           
+            let p1=0;
+            p1 = 0 + 1;
+            display.innerText = `${player1.value} you win this round`
+            ini = "";
+            playr11.innerHTML = p2;
+        }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O") || (a == "O" && f == "O" && j == "O") || (c == "O" && f == "O" && h == "O")) {
+            let p2=0;
+            p2 = 0 + 1;
+            display.innerText = `${player2.value} you win this round`;
+            ini = "";
+            playr22.innerHTML = p2;
+
+        }
+
+    }else{
+        ready.innerText = "Please provide your name"
     }
 }
