7fcf37b9da28ecba00df8490c4b1d876aee34c85
debo <ogunweoluwadebo@gmail.com>
2022-07-19 17:05:36 +0100

    done

--- tic.js
+++ tic.js
@@ -44,7 +44,6 @@ const starts = ()=>{
         nam22.innerHTML = `${player2.value}`
     }else{
         alert("Please provide your name")
-        // ready.innerText = "Please provide your name"
     }
 }
 
@@ -72,7 +71,7 @@ const sa = (e)=>{
         // check winning
         if ((a == "X" && b == "X" && c == "X") || (d == "X" && f == "X" && g == "X") || (h == "X" && i == "X" && j == "X") || (a == "X" && d == "X" && h == "X") || (b == "X" && f == "X" && i == "X") || (c == "X" && g == "X" && j == "X") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
           setTimeout(()=>{
-            display.innerText = `${player1.value} win this round`
+            alert(`${player1.value} win this round`)
             empty();
             ini = "";
             s1 = s1+1;
@@ -82,7 +81,7 @@ const sa = (e)=>{
           },500)
         }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O") || (a == "O" && f == "O" && j == "O") || (c == "O" && f == "O" && h == "O")) {
             setTimeout(()=>{
-                display.innerText = `${player2.value} win this round`;
+                alert(`${player2.value} win this round`)
                 empty();
                 ini = "";
                 d1 = d1+1;
@@ -93,7 +92,6 @@ const sa = (e)=>{
         }
     }else{
         alert("Please provide your name")
-        // ready.innerText = "Please provide your name"
     }
 }
 const resect = ()=>{
