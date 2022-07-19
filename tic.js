0868d33fb95430daa994aa70f53cc833de04177a
debo <ogunweoluwadebo@gmail.com>
2022-07-19 16:52:22 +0100

    score taken sucessfully

--- tic.js
+++ tic.js
@@ -15,7 +15,18 @@ let sixs = document.getElementById("six");
 let sevens = document.getElementById("seven");
 let eights = document.getElementById("eight");
 let nines = document.getElementById("nine");
-
+const empty=()=>{
+    ini = "X";
+    ones.innerHTML="";
+    twos.innerHTML="";
+    threes.innerHTML="";
+    fours.innerHTML="";
+    fives.innerHTML="";
+    sixs.innerHTML="";
+    sevens.innerHTML="";
+    eights.innerHTML="";
+    nines.innerHTML="";
+}
 const restarts = ()=>{
     let anser = confirm('Are you sure u want to restart');
     if (anser === true) {
@@ -29,8 +40,11 @@ const starts = ()=>{
         playr2.innerText = `${player2.value} you are player 2`
         nam1.innerHTML = `${player1.value}`
         nam2.innerHTML = `${player2.value}`
+        nam11.innerHTML = `${player1.value}`
+        nam22.innerHTML = `${player2.value}`
     }else{
-        ready.innerText = "Please provide your name"
+        alert("Please provide your name")
+        // ready.innerText = "Please provide your name"
     }
 }
 
@@ -57,18 +71,29 @@ const sa = (e)=>{
         let j = nines.innerHTML;
         // check winning
         if ((a == "X" && b == "X" && c == "X") || (d == "X" && f == "X" && g == "X") || (h == "X" && i == "X" && j == "X") || (a == "X" && d == "X" && h == "X") || (b == "X" && f == "X" && i == "X") || (c == "X" && g == "X" && j == "X") || (a == "X" && f == "X" && j == "X") || (c == "X" && f == "X" && h == "X")) {
+          setTimeout(()=>{
             display.innerText = `${player1.value} win this round`
+            empty();
             ini = "";
             s1 = s1+1;
             scor1.innerHTML=s1;
+            scor11.innerHTML=s1;
+            return;
+          },500)
         }else if ((a == "O" && b == "O" && c == "O") || (d == "O" && f == "O" && g == "O") || (h == "O" && i == "O" && j == "O") || (a == "O" && d == "O" && h == "O") || (b == "O" && f == "O" && i == "O") || (c == "O" && g == "O" && j == "O") || (a == "O" && f == "O" && j == "O") || (c == "O" && f == "O" && h == "O")) {
-            display.innerText = `${player2.value} win this round`;
-            ini = "";
-            d1 = d1+1;
-            scor2.innerHTML=d1;
+            setTimeout(()=>{
+                display.innerText = `${player2.value} win this round`;
+                empty();
+                ini = "";
+                d1 = d1+1;
+                scor2.innerHTML=d1;
+                scor22.innerHTML=d1;
+                return;
+            },500)
         }
     }else{
-        ready.innerText = "Please provide your name"
+        alert("Please provide your name")
+        // ready.innerText = "Please provide your name"
     }
 }
 const resect = ()=>{
